b5012f9fbbf0e03f5bb235b4c3c9dd4bfc01117f
Rcolet <remi.colet@icloud.com>
2018-04-19 14:06:21 +0200

    If user no have file VERSION in root project, user choice if command create file VERSION with 0.0.0

--- src/Composer/Command/VersionCommand.php
+++ src/Composer/Command/VersionCommand.php
@@ -14,7 +14,7 @@ class VersionCommand extends BaseCommand
 
     protected $NAME_VERSION_FILE = 'VERSION';
     protected $NAME_GIT_FOLDER = '.git';
-    protected $VERSION_PROJECT = "0.0.1";
+    protected $VERSION_PROJECT = "0.0.0";
 
     protected function configure()
     {
@@ -34,7 +34,7 @@ EOT
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $io = new SymfonyStyle($input, $output);
-        $return_path = $input->getOption("path");
+        $return_path = $input->getOption("root");
         $project_path = substr($return_path, -1) == '/' ? $return_path : $return_path.'/';
 
         if(!file_exists($project_path."composer.json")) {
@@ -42,10 +42,8 @@ EOT
             exit(1);
         }
 
-        //exit(var_dump(preg_match('/^[0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2}/', $input->getArgument('typeUp'))));
-
         if(file_exists($project_path.$this->NAME_VERSION_FILE)) {
-            $this->VERSION_PROJECT = exec("cat VERSION");
+            $this->VERSION_PROJECT = exec("cat ".$project_path.$this->NAME_VERSION_FILE);
             if(in_array($input->getArgument('typeUp'), array('major', 'minor', 'patch')) || preg_match('#^[0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2}#', $input->getArgument('typeUp'))) {
                 $explode_version = explode('.', $this->VERSION_PROJECT);
                 switch ($input->getArgument('typeUp')) {
@@ -68,17 +66,19 @@ EOT
                 }
                 $output->writeln("Old version project : ".$this->VERSION_PROJECT);
                 $this->VERSION_PROJECT = implode('.', $explode_version);
-                exec("echo ".$this->VERSION_PROJECT." > ".$this->NAME_VERSION_FILE);
-                // $version_file = fopen($this->NAME_VERSION_FILE, 'w+');
-                // fwrite($version_file, $this->VERSION_PROJECT);
-                // fclose($version_file);
+                exec("echo ".$this->VERSION_PROJECT." > ".$project_path.$this->NAME_VERSION_FILE);
                 $output->writeln("New version project : ".$this->VERSION_PROJECT);
             } else {
                 $output->writeln("Project version : ".$this->VERSION_PROJECT);
             }
         } else {
             $output->writeln("File VERSION not exist");
-            //$io->choice("Create this file ? ", array("yes", "no"), "yes");
+            $anwser = $io->choice("Create this file ? ", array("yes", "no"), "yes");
+            if($anwser === "yes") {
+                exec("echo ".$this->VERSION_PROJECT. " > ".$project_path.$this->NAME_VERSION_FILE);
+            }
+
+            $output->writeln("THE END!!!");
         }
     }
 }
