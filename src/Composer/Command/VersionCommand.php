cb0fc7340480018874d06cae6b1fc79767197f39
Rcolet <remi.colet@icloud.com>
2018-04-20 14:40:26 +0200

    Change open file function

--- src/Composer/Command/VersionCommand.php
+++ src/Composer/Command/VersionCommand.php
@@ -39,31 +39,32 @@ EOT
         $return_root = $input->getOption("root");
         $project_root = substr($return_root, -1) == '/' ? $return_root : $return_root.'/';
 
-        if(!file_exists($project_root."composer.json")) {
-            $output->writeln("You are not aiming Composer project");
-            exit(1);
+        if(!file_exists($project_root.$this->NAME_GIT_FOLDER)) {
+            $output->writeln("This is not GIT repository !");
+            exit(502);
         }
 
         if(file_exists($project_root.$this->NAME_VERSION_FILE)) {
-            $this->VERSION_PROJECT = exec("cat ".$project_root.$this->NAME_VERSION_FILE);
+            $this->VERSION_PROJECT = file_get_contents($project_root.$this->NAME_VERSION_FILE);
             if($input->getArgument('new-version')){
                 if(preg_match('#^[0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2}(-[[:graph:]]+[[:alnum:]]){0,1}#', $input->getArgument('new-version'))) {
                     $output->writeln("Old version project : ".$this->VERSION_PROJECT);
                     $this->VERSION_PROJECT = $input->getArgument('new-version');
                 } else {
+                    $output->writeln("test 1");
                     $explode_version = explode('.', $this->VERSION_PROJECT);
                     switch ($input->getArgument('new-version')) {
                         case 'major':
-                            $explode_version[0]++;
+                            $explode_version[0] = ((int)$explode_version[0])+1;
                             $explode_version[1] = '0';
                             $explode_version[2] = '0';
                             break;
                         case 'minor':
-                            $explode_version[1]++;
+                            $explode_version[1] = ((int)$explode_version[1])+1;
                             $explode_version[2] = '0';
                             break;
                         case 'patch':
-                            $explode_version[2]++;
+                            $explode_version[2] = ((int)$explode_version[2])+1;
                             break;
                         default:
                             $output->writeln("ERROR : major|minor|patch or a direct version like 0.0.1");
@@ -71,17 +72,18 @@ EOT
                             break;
                     }
                     $output->writeln("Old version project : ".$this->VERSION_PROJECT);
-                $this->VERSION_PROJECT = implode('.', $explode_version);
+                    $this->VERSION_PROJECT = implode('.', $explode_version);
                 }
-                exec("echo ".$this->VERSION_PROJECT." > ".$project_root.$this->NAME_VERSION_FILE);
+                exit(var_dump($this->VERSION_PROJECT));
+                file_put_contents($project_root.$this->NAME_VERSION_FILE, $this->VERSION_PROJECT);
                 $output->writeln("New version project : ".$this->VERSION_PROJECT);
+
+                // exec("cd .$project_root; git add $this->NAME_GIT_FOLDER; git commit -m \"New Version : $this->VERSION_PROJECT\"; ");
+
             } else {
                 $output->writeln("Project version : ".$this->VERSION_PROJECT);
             }
         } else {
-            if(file_exists($project_root.$this->NAME_GIT_FOLDER)) {
-                $output->writeln("Git repo found : under development!!!");
-            }
 
             if($input->getArgument('new-version')){
                 if(preg_match('#^[0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2}(-[[:graph:]]+[[:alnum:]]){0,1}#', $input->getArgument('new-version'))) {
@@ -94,6 +96,7 @@ EOT
             $anwser = $io->choice("Create this file ? [".$this->VERSION_PROJECT."]", array("yes", "no"), "yes");
             if($anwser === "yes") {
                 exec("echo ".$this->VERSION_PROJECT. " > ".$project_root.$this->NAME_VERSION_FILE);
+                $output->writeln("It will be created !");
             }
         }
 
